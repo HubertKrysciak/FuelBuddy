444c1654b1cf45320da8d9956269b00c951ba2ae
Hubert Kryściak <hubert.krysciak@gmail.com>
2024-09-18 21:00:47 +0200

    Init

---
+++ src/main/java/pl/hubs/fuelbuddy/FuelBuddyApplication.java
@@ -0,0 +1,13 @@
+package pl.hubs.fuelbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuelBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/hubs/fuelbuddy/FuelBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hubs.fuelbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
